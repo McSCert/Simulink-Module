5ab88fdac164e073a7438ef8c42909bbeae70d4f
Gordon Marks <gor.marks@gmail.com>
2018-06-05 18:04:21 +0000

    Bug fix to gcbs when gcb is empty (this error probably wasn't an issue since gcb is generally expected to not be empty).
    git-svn-id: https://www.mcscert.ca/leap/svn/trunk/sw/tools/Simulink/Tools/FunctionHelper@12954 9a9db257-bd8a-4a57-83e7-af9ab8ab1356

--- Utility/gcbs.m
+++ Utility/gcbs.m
@@ -3,9 +3,14 @@ function sels = gcbs
     %returns a cell array of all currently selected blocks
     %limited to the subsystem established by GCB.
     %C. Hecker/11Dec06
-    ssys = get_param(gcb, 'parent');
-    nBlks = find_system(ssys,'SearchDepth',1, 'LookUnderMasks', 'all', 'FollowLinks', 'on');
-    nBlks = nBlks(2:end); %strip off parent system name
-    idx = strcmp(get_param(nBlks, 'selected'), 'on');
-    sels = nBlks(idx);
+    if isempty(gcb)
+        sels = {};
+    else
+        ssys = get_param(gcb, 'parent');
+        nBlks = find_system(ssys,'SearchDepth',1, 'LookUnderMasks', 'all', 'FollowLinks', 'on');
+        nBlks = nBlks(2:end); %strip off parent system name
+        idx = strcmp(get_param(nBlks, 'selected'), 'on');
+        sels = nBlks(idx);
+    end
 end
+

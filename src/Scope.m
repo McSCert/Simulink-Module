90a42187b2fbf4f20caca69041bcecd5b00321a8
Stephen Scott <stephen99scott@gmail.com>
2020-08-11 12:14:58 -0400

    Removed duplicate utility files and updated the utility submodule

--- src/Scope.m
+++
@@ -1,8 +0,0 @@
-classdef Scope < uint32
-    % Scope values, for readability
-   enumeration
-      Global (0)    % Function visibility: global
-      Scoped (1)    % Function visibility: scoped; Placement: root
-      Local  (2)    % Function visibility: scoped; Placement: subsystem
-   end
-end
